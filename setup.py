1ff727df3fd227a3a424b7f2a69834a039719dbf
Cass Chin <casschin@gmail.com>
2015-09-23 15:43:08 -0400

    name change

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="appium-screen",
+    name="screenium",
     version="0.1.0",
     author="Cass Chin",
     author_email="casschin@gmail.com",
     description="Python wrapper for Appium",
     license="MIT",
     keywords="python appium selenium",
-    url="https://github.com/casschin/appium-screen",
+    url="https://github.com/casschin/screenium",
     packages=find_packages(),
     install_requires=["selenium", "Appium-Python-Client"]
 )
